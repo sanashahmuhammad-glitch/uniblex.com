820cfa6c0653ee7037e83eb576f5db625633cf1a
sanashahmuhammad-glitch <sanashahmuhammad@gmail.com>
2026-07-12 02:30:49 +0500

    Bust Moto Rider iframe cache

--- src/lib/gameIframeUrls.ts
+++ src/lib/gameIframeUrls.ts
@@ -1,4 +1,4 @@
 ﻿export const MOTO_RIDER_SLUG = "moto-rider-3d-bike-race-game";
-export const MOTO_RIDER_IFRAME_URL = "https://games.uniblex.com/bike-game/index.html?v=20260711-2";
+export const MOTO_RIDER_IFRAME_URL = "https://games.uniblex.com/bike-game/index.html?v=20260712-3";
 export const MOTO_RIDER_THUMBNAIL_URL = "https://games.uniblex.com/bike-game/thumbnail.png?v=2";
 export const MOTO_RIDER_COVER_URL = "https://games.uniblex.com/bike-game/cover-image.png?v=2";
